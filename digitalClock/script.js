35bbe72b1f1dd335ddc25281a1ee53fbc6b9f134
TundeAkintola <150985943+TundeAkintola@users.noreply.github.com>
2024-01-23 21:57:05 +0100

    Create script.js

---
+++ digitalClock/script.js
@@ -0,0 +1,57 @@
+const hourEl = document.getElementById("hour")
+
+const minuteEl = document.getElementById("minute")
+
+const secondEl = document.getElementById("second")
+
+const ampmEl = document.getElementById("ampm")
+
+
+
+function updateClock() {
+
+    let h = new Date().getHours()
+
+    let m = new Date().getMinutes()
+
+    let s = new Date().getSeconds()
+
+    let ampm = "PM"
+
+
+
+    if (h > 12) {
+
+        h = h - 12
+
+        ampm = "AM"
+
+    }
+
+
+
+    h = h < 10 ? "0" + h : h;
+
+    m = m < 10 ? "0" + m : m;
+
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+
+    minuteEl.innerText = m;
+
+    secondEl.innerText = s;
+
+    ampmEl, (innerText = ampm);
+
+    setTimeout(() => {
+
+        updateClock()
+
+    }, 1000);
+
+}
+
+
+
+updateClock()
