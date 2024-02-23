fc656efea5b9531170e114e5a3e8b82f8380e259
TundeAkintola <150985943+TundeAkintola@users.noreply.github.com>
2024-02-23 02:22:50 +0100

    Create script.js

---
+++ guessMyNumber/script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const check = document.querySelector('.check');
+const again = document.querySelector('.again');
+const secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// console.log((document.querySelector('.number').textContent = secretNumber));
+
+let score = 10;
+let highscore = 0;
+let guess = Number(document.querySelector('.guess').value);
+
+function guessChecker() {
+  const guess = Number(document.querySelector('.guess').value);
+  // if there is no input
+  if (!guess) {
+    console.log(
+      (document.querySelector('.message').textContent = '⛔ No Number!')
+    );
+
+    document.querySelector('.screen').style.backgroundColor = '#ff010b';
+  }
+  // Game over
+  else if (score < 1) {
+    document.querySelector('.message').textContent = `Game Over 😂, Press
+    'Again'
+    Button`;
+  }
+  // if the guessed number matches the secretNumber
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    score++;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.screen').style.backgroundColor = '#108010';
+    // after 20 points the highscore increases by 1
+    if (guess > highscore) {
+      highscore++;
+      document.querySelector('.highscore').textContent = score;
+    }
+  }
+  // // if the guess is greater than the secretNumber or greater than 20
+  else if (guess > secretNumber) {
+    document.querySelector(
+      '.message'
+    ).textContent = `❌ Wrong! Too High!, Choose from 1-10`;
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector('.screen').style.backgroundColor = '#e6080e';
+  }
+
+  //if the guessed number is less than the secretNumber
+  else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = ` ❌ Wrong ,Too Low!`;
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector('.screen').style.backgroundColor = '#e6080e';
+  }
+}
+
+function reset() {
+  document.querySelector('.screen').style.backgroundColor = '#03e708';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing....';
+  document.querySelector('.score').textContent = '10';
+  document.querySelector('.highscore').textContent = '0';
+}
+
+function listeners() {
+  check.addEventListener('click', guessChecker);
+  again.addEventListener('click', reset);
+}
+
+listeners();
+ 
